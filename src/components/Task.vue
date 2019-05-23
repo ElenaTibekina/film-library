f034fa19e78876b1f24991b09d176cb244fddba5
Elena Tibekina <elentibekina@gmail.com>
2019-05-23 17:02:41 +0300

    added vuex

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -29,28 +29,11 @@
 
 <script>
 export default {
-    data() {
-        return {
-            tasks: [
-                {
-                    'id': 1,
-                    'title': 'GrowthBusters: Hooked on Grouth',
-                    'description': 'I directed this documentary chalinging lalalal lalalalal lalalajhbcbjhj lskksjshhshsh ksksjhshj ksksjjsj hfhjhd hdsh bbbdsgdgsjgdj gdgdggds bsbsgg bsbsb',
-                    'whatWhatch': 'film',
-                    'completed': false,
-                    'editing': false
-                },
-                {
-                    'id': 2,
-                    'title': 'Game of Thrones',
-                    'description': 'The best TV-show',
-                    'whatWhatch': 'TV-show',
-                    'completed': false,
-                    'editing': false
-                }
-            ]
-        }
+  computed: {
+    tasks () {
+      return this.$store.getters.tasks
     }
+  }
 }
 </script>
 
