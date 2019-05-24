719d1a87800fd3d1146005679017b3deadbadef0
Elena Tibekina <elentibekina@gmail.com>
2019-05-24 23:07:59 +0300

    inputs email, password, repeatPassword validated

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,37 +1,65 @@
 <template lang="pug">
-    .content-wraper
-        section
-            .container
-              .auth
-                .auth__banner
-                  h1.ui-title-2 Hello banner
-                .auth__form
-                  span.ui-title-2 Registration
-                    form
-                      input(
-                        type="email"
-                        placeholder="Email"
-                        v-model="email"
-                      )
-                      input(
-                        type="password"
-                        placeholder="Password"
-                        v-model="password"
-                      )
-                      input(
-                        type="password"
-                        placeholder="Repeat your password"
-                        v-model="repeatPassword"
-                      )
+  .content-wrapper
+    section
+      .auth
+        .auth__banner
+          h1.ui-title-2 Hello banner
+        .auth__form
+          span.ui-title-2 Registration
+          form
+            .form-item(:class="{ errorInput: $v.email.$error }")
+              input(
+                type="email"
+                placeholder="Email"
+                v-model="email"
+                :class="{ error: $v.email.$error }"
+                @change="$v.email.$touch()"
+              )
+              .error(v-if="!$v.email.required") Field is required
+              .error(v-if="!$v.email.email") Email is not correct
+            .form-item(:class="{ errorInput: $v.password.$error }")
+              input(
+                type="password"
+                placeholder="Password"
+                v-model="password"
+                :class="{ errorInput: $v.password.$error }"
+                @change="$v.password.$touch()"
+              )
+              .error(v-if="!$v.password.required") Password is required.
+              .error(v-if="!$v.password.minLength")
+                | Password must have at least {{ $v.password.$params.minLength.min }} letters.
+            .form-item(:class="{ errorInput: $v.repeatPassword.$error }")
+              input(
+                type="password"
+                placeholder="Repeat your password"
+                v-model="repeatPassword"
+                :class="{ errorInput: $v.repeatPassword.$error }"
+                @change="$v.repeatPassword.$touch()"
+              )
+              .error(v-if="!$v.repeatPassword.sameAsPassword") Passwords must be identical.
 </template>
 
 <script>
+import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
 export default {
   data () {
     return {
       email: '',
       password: '',
       repeatPassword: ''
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    },
+    repeatPassword: {
+      sameAsPassword: sameAs('password')
     }
   }
 }
@@ -43,4 +71,18 @@ export default {
 .auth__banner,
 .auth__form
   width 50%
+
+.form-item
+  .error
+    display none
+    margin-bottom 8px
+    font-size 13.4px
+    color #fc5c65
+  &.errorInput
+    .error
+      display block
+
+input
+  &.error
+    border-color #fc5c65
 </style>
